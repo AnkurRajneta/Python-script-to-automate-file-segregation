7db6de89c93678d1199659c77e89f8e6f6f87aa6
AnkurRajneta <ankur_207003@saitm.org>
2023-04-02 19:14:56 +0530

    updated great.py

--- great.py
+++ great.py
@@ -1,10 +1,10 @@
 import os
-path = "C:/great"
+path = "C:great"
 os.startfile(path)
 l = []
 for root,directories,file in os.walk(path):
         for files in file:
-                l.append(os.path.join(root,file))
+                l.append(os.path.join(root,file) for j in l)
 
 for f in l:
         print(f)
