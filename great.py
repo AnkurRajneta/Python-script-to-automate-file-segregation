cfb481e73116f1ce5f7456f2cbcebca3b51575df
AnkurRajneta <ankur_207003@saitm.org>
2023-04-06 00:52:47 +0530

    updated file

--- great.py
+++ great.py
@@ -4,22 +4,25 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import pandas as pd
 driver = webdriver.Chrome()
-driver.get('https://gtyy.com')
-
-file_path = "C:/great"
-
-pattern = r'([^/\\]+)(?:\.\w+)?\s+from'
+driver.get('https://chromium')
 
 
-for path,directories,files in os.walk(file_path):
 
-    for name in files:
-        jk = os.path.join(path,name)
+# pattern = r'([^/\\]+)(?:\.\w+)?\s+from'
 
-        result = re.search(pattern, jk)
-        if result:
-            extracted_string = result.group(1)
-            print(extracted_string)
 
-       
+# for path,directories,files in os.walk(file_path):
+
+#     for name in files:
+#         jk = os.path.join(path,name)
+
+#         result = re.search(pattern, jk)
+#         if result:
+#             extracted_string = result.group(1)
+#             print(extracted_string)
+
+Examples = driver.find_elements_by_xpath()    
+Examples_list = []
+for p in range(len(Examples)):
+    Examples_list.append(Examples[p].text)
 
