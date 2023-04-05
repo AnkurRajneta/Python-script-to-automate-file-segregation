da1fcbfb2f3db0adac3a38dab939069118c9f0ed
AnkurRajneta <ankur_207003@saitm.org>
2023-04-06 00:11:12 +0530

    updated file

--- great.py
+++ great.py
@@ -1,9 +1,14 @@
 import os
 import re
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import pandas as pd
+driver = webdriver.Chrome()
+driver.get('https://gtyy.com')
 
 file_path = "C:/great"
 
-pattern = r'('
+pattern = r'([^/\\]+)(?:\.\w+)?\s+from'
 
 
 for path,directories,files in os.walk(file_path):
