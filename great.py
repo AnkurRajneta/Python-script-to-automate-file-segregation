b8ed4ccc14bcfe1034f4f2e46abbc360279e980d
AnkurRajneta <ankur_207003@saitm.org>
2023-04-15 18:38:45 +0530

    updated file

--- great.py
+++ great.py
@@ -2,13 +2,16 @@ import os
 import re
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
 import pandas as pd
 driver = webdriver.Chrome()
-driver.get('https://chromium')
+driver.get('https://google.com')
 
 
 
-# pattern = r'([^/\\]+)(?:\.\w+)?\s+from'
+
 
 
 # for path,directories,files in os.walk(file_path):
@@ -21,8 +24,9 @@ driver.get('https://chromium')
 #             extracted_string = result.group(1)
 #             print(extracted_string)
 
-Examples = driver.find_elements_by_xpath()    
-Examples_list = []
-for p in range(len(Examples)):
-    Examples_list.append(Examples[p].text)
 
+wait = WebDriverWait(driver, 10)
+
+# Find the element by class name
+element_list = []
+element = wait.until(EC.presence_of_element_located((By.CLASS_NAME, "workingit"))) 
